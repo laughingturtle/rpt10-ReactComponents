d97b555a76fb7d0be7dcc9a2499f158737258883
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-09-28 21:50:09 -0700

    Complete creating the grocery list component

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // TODO
+var GroceryList = () => (
+    <div>
+    <h2>My Grocery List</h2>
+    <li>Carrot</li>
+    <li>Kale</li>
+  </div>
+);
+
+// var Cucumbers = () => (
+
+// );
+
+// var Kale = () => (
+
+// );
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
